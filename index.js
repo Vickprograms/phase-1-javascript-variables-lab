98d64daf54ca45431deecf5504e6900f3604ef82
Victor <victormugo08@gmail.com>
2025-03-04 10:50:25 +0300

    Modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+let Scuber = companyName;
+
+const Chelsea = mostProfitableNeighborhood;
+let mostProfitableNeighborhood = 'Chelsea';
+
+const SusanSmith = companyCeo;
+let companyCeo = 'Susan Smith';
